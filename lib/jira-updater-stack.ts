541e5df70a92738a3e7ea6aa13cf9f33201f551f
devinreeks <devin@gotillit.com>
2021-10-19 11:17:31 +1000

    second commit

--- lib/jira-updater-stack.ts
+++ lib/jira-updater-stack.ts
@@ -1,18 +1,33 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as iam from '@aws-cdk/aws-iam';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import path = require('path');
 
 export class JiraUpdaterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'JiraUpdaterQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const lambdaRole = new iam.Role(this, 'lambdaRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
+    })
+    lambdaRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'))
+    const lambdaServer = new lambda.Function(this, 'lambdaApiServer', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      role: lambdaRole,
+      handler: 'jiraLambda.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/lambdaServer/')),
+      timeout: cdk.Duration.seconds(300)
+    })
+    const userServer = new lambda.Function(this, 'lambdaUserServer', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      role: lambdaRole,
+      handler: 'lambdaUserServer.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/lambdaUserServer')),
+      timeout: cdk.Duration.seconds(300)
+    })
+    const table = new dynamodb.Table(this, 'Table', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableName: 'gitlab-hook-table'
     });
-
-    const topic = new sns.Topic(this, 'JiraUpdaterTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
